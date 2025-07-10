c26a106601d44477ac188e1c829b7e46683cdaee
Tom <tomas95go@gmail.com>
2025-07-10 20:13:48 -0300

    green: should play 10 frames when game plays. refactor: frame increment is controlled by the game, not by the player

--- src/tests/game.test.ts
+++ src/tests/game.test.ts
@@ -4,7 +4,7 @@ class Game {
   readonly player: string;
   readonly score: number;
   readonly frames: number = 10;
-  currentFrame: number = 0;
+  currentFrame: number = 1;
 
   static initialize(player: string): Game {
     return new Game(player, 0);
@@ -13,6 +13,13 @@ class Game {
   constructor(player: string, score: number) {
     this.player = player;
     this.score = score;
+  }
+
+  play(): void {
+    let frame = this.currentFrame;
+    for (frame; frame <= this.frames; frame++) {
+      this.currentFrame = frame;
+    }
   }
 
   playerThrowsBall(): void {
@@ -32,13 +39,13 @@ describe('game module', () => {
     expect(game.currentFrame).toBe(0);
   });
 
-  it('should increment frame by 1 when player throw ball', () => {
+  it('should play 10 frames when game plays', () => {
     const player = 'Ryu';
 
     const game = Game.initialize(player);
 
-    game.playerThrowsBall();
+    game.play();
 
-    expect(game.currentFrame).toBe(1);
+    expect(game.currentFrame).toBe(10);
   });
 });
